a7de93a3bc8748b2f4465d43575eb324e47be6f4
logonrm <logonrm@fiap.com.br>
2018-03-02 21:06:17 -0300

    exercicio 1 - Introducao JPA

--- 04-JPA-API/src/br/com/fiap/aula4/dao/impl/CarroDAOImpl.java
+++ 04-JPA-API/src/br/com/fiap/aula4/dao/impl/CarroDAOImpl.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 
 import br.com.fiap.aula04.entities.Carro;
 import br.com.fiap.aula4.dao.CarroDAO;
+import br.com.fiap.aula4.exceptions.CarNotFoundException;
+import br.com.fiap.aula4.exceptions.CommitException;
 
 public class CarroDAOImpl implements CarroDAO {
 
@@ -17,27 +19,41 @@ public class CarroDAOImpl implements CarroDAO {
 	@Override
 	public void create(Carro carro) {
 		em.persist(carro);
-		em.getTransaction().begin();
-		em.getTransaction().commit();
 	}
 
 	@Override
 	public void update(Carro carro) {
 		em.merge(carro);
-		em.getTransaction().begin();
-		em.getTransaction().commit();
 	}
 
 	@Override
-	public void delete(int codigo) {
-		em.remove(em.find(Carro.class, codigo));
-		em.getTransaction().begin();
-		em.getTransaction().commit();
+	public void delete(int codigo) throws CarNotFoundException {
+		Carro carro = em.find(Carro.class, codigo);
+		
+		if (carro == null) {
+			throw new CarNotFoundException("Registro não encontrado.");
+		}
+		
+		em.remove(carro);
 	}
 
 	@Override
 	public Carro find(int codigo) {
 		return em.find(Carro.class, codigo);
 	}
-	
+
+	@Override
+	public void commit() throws CommitException {
+		try {
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			e.printStackTrace();
+			throw new CommitException("Erro no commit.");
+		}
+	}
+
 }
